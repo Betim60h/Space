0c0bde520d4ac4d1cee16fdcea6a4cd4afb1f4a8
Roberto Jacobs <1135584@atitus.edu.br>
2023-11-21 01:57:53 -0300

    Space um

---
+++ space.py
@@ -0,0 +1,39 @@
+import pygame
+import sys
+from tkinter import simpledialog
+
+
+def play_music():
+    pygame.mixer.init()
+    pygame.mixer.music.load('space.mp3')
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play(-1)
+
+pygame.init()
+tamanho = (1280, 720)
+clock = pygame.time.Clock()
+tela = pygame.display.set_mode(tamanho)
+fundo = pygame.image.load("space.jpg").convert()  
+icone = pygame.image.load("space.png")
+pygame.display.set_icon(icone)
+pygame.display.set_caption("Space")
+fundo = pygame.transform.scale(fundo, tamanho)
+play_music ()
+
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif evento.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            item = simpledialog.askstring("Space", "Digite um valor para 'Space'")
+            print(item)
+            if item is None:
+                item = "desconhecido" + str(pos)
+                estrelas[item] = pos
+    tela.blit(fundo, (0, 0))  
+    pygame.display.flip()
+
+    clock.tick(60)
